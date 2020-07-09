b47e3a2905df18e0f702c8a9a155e2961ac3613e
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-07-09 13:02:05 -0500

    Add http service (does not work)

--- WebDev/React/shop-web/src/app/App.js
+++ WebDev/React/shop-web/src/app/App.js
@@ -1,27 +1,54 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HttpService from '../services/htttp-service';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome to EA-SHOP</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const http = new HttpService();
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    console.log("Suppose I make an http request");
+
+    // We need to bind functions
+    this.loadData = this.loadData.bind(this);
+    this.loadData();
+    //http.getProducts();
+  }
+
+
+  loadData = () => {
+    http.getProducts().then(products => {
+      console.log(products);
+    }, err => {
+      console.log("Something went wrong!");
+    });
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Welcome to EA-SHOP</h1>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
