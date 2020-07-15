6b57d1bbe21c317bf1633d3a2e5d6c56998d4900
ea-maruri <64037074+ea-maruri@users.noreply.github.com>
2020-07-15 12:56:17 -0500

    Create blog, posts, menus

---
+++ WebDev/Basic_Blog/data.php
@@ -0,0 +1,29 @@
+<?php
+
+$menus = [
+    ["title" => "Home", "url" => "/"],
+    ["title" => "About", "url" => "#about"],
+    ["title" => "Gallery", "url" => "#gallery"],
+    ["title" => "Contact", "url" => "#contact"]
+];
+
+$posts = [
+  [
+    "title" => "Demo title 1",
+    "intro" => "This is simple content and its cool",
+    "image" => "https://picsum.photos/150/150/",
+    // Task 1 add author
+    "author" => "",
+    // Task 2 add date
+    "date" => ""
+  ],
+  [
+    "title" => "Demo title 2",
+    "intro" => "This is simple content as second article and its cool",
+    "image" => "https://picsum.photos/150/150/",
+    // Task 1 add author
+    "author" => "",
+    // Task 2 add date
+    "date" => ""
+  ]
+];
